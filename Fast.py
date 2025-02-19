fcee120edb579951db7e6d50e2fcba0b7f4e58a1
chetan-1487 <chetan@thinksys.com>
2025-02-19 12:18:11 +0530

    change

--- Fast.py
+++ Fast.py
@@ -1,35 +1,34 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from moviepy import VideoFileClip
-import uuid
-from fastapi.responses import FileResponse
 import os
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-@app.post("/extract-audio/")
+@app.post("/extract-audio")
 async def extract_audio(file: UploadFile = File(...)):
     if not file.content_type.startswith("video/"):
-        raise HTTPException(status_code=400,detail="File must be a video")
-    video=f"temp_{uuid.uuid4()}.mp4"
-    audio=f"temp_{uuid.uuid4()}.mp3"
+        raise HTTPException(status_code=400, detail="File must be a video")
+
+    video_filename ="video_filename.mp4"
+    audio_filename = "audio_filename.mp3"
+
     try:
-        with open(video, "wb") as video_file:
+        with open(video_filename, "wb") as video_file:
             video_file.write(await file.read())
 
-        video_clip = VideoFileClip(video)
+        if not os.path.exists(video_filename):
+            raise HTTPException(status_code=500, detail="Error: Video file was not saved.")
+        video_clip = VideoFileClip(video_filename)
         audio_clip = video_clip.audio
-        audio_clip.write_audiofile(audio)
+        audio_clip.write_audiofile(audio_filename)
 
-        video_clip.close()
-        audio.close()
+        if not os.path.exists(audio_filename):
+            raise HTTPException(status_code=500, detail="Error: Audio file was not generated.")
 
-        return FileResponse(audio, media_type="audio/mpeg", filename=audio)
-    
+        response = FileResponse(audio_filename, media_type="audio/mpeg", filename=audio_filename)
+
+        return response
+
     except Exception as e:
-        raise HTTPException(status_code=500,detail=f"error procssing in video")
-    
-    finally:
-        if os.path.exists(video):
-            os.remove(video)
-        if os.path.exists(audio):
-            os.remove(audio)
+        raise HTTPException(status_code=500, detail="error")
