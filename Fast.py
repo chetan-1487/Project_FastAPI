f674b2b196729ba8b33c9dc624439089ee747b74
chetan-1487 <chetan@thinksys.com>
2025-02-19 10:13:18 +0530

    New_Changes

--- Fast.py
+++ Fast.py
@@ -1,29 +1,35 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from moviepy import VideoFileClip
-import tempfile
+import uuid
 from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
 @app.post("/extract-audio/")
 async def extract_audio(file: UploadFile = File(...)):
+    if not file.content_type.startswith("video/"):
+        raise HTTPException(status_code=400,detail="File must be a video")
+    video=f"temp_{uuid.uuid4()}.mp4"
+    audio=f"temp_{uuid.uuid4()}.mp3"
     try:
-        temp_video_path = tempfile.mktemp(suffix=".mp4")
-
-        with open(temp_video_path, "wb") as video_file:
+        with open(video, "wb") as video_file:
             video_file.write(await file.read())
 
-        video_clip = VideoFileClip(temp_video_path)
-
-        audio = video_clip.audio
-
-        temp_audio_path = tempfile.mktemp(suffix=".mp3")
-        audio.write_audiofile(temp_audio_path)
+        video_clip = VideoFileClip(video)
+        audio_clip = video_clip.audio
+        audio_clip.write_audiofile(audio)
 
         video_clip.close()
         audio.close()
 
-        return FileResponse(temp_audio_path, media_type="audio/mp3", filename="extracted_audio.mp3")
+        return FileResponse(audio, media_type="audio/mpeg", filename=audio)
     
     except Exception as e:
-        raise HTTPException()
+        raise HTTPException(status_code=500,detail=f"error procssing in video")
+    
+    finally:
+        if os.path.exists(video):
+            os.remove(video)
+        if os.path.exists(audio):
+            os.remove(audio)
